0f4f16e6daa21333fb4b3c95b25f92a82a5beb4f
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2024-03-13 16:43:57 -0300

    apenas brincando

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,6 +18,20 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/teste', function (){
+    return response('Hello world', 200);
+});
+
+Route::get('/numero/{numero}', function ($numero){
+    return preg_match('/^[0-9]*$/', $numero) ? response('O numero inserido é: '. $numero) : response('Invalido', 404);
+});
+
+Route::get('/query', function (Request $request){
+   return response($request->nome . ' ' . $request->cidade, 200);
+});
+
+//-> where('numero','[0-9]+');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
